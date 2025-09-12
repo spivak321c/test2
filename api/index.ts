8da327d7721f41bad1af23dd083651b10a8a4676
simon <spivakNak2@proton.me>
2025-09-12 23:55:50 +0000

    vercel new25

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import app from '../src/index'; // import your Express app
+import app from '../dist/index'; // import your Express app
 
 // Vercel handler that delegates to Express
 export default function handler(req: VercelRequest, res: VercelResponse) {
