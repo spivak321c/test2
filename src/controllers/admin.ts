288c94375c674ff14921fa89433f5874bff02eee
simon <spivakNak2@proton.me>
2025-09-13 14:43:35 +0000

    admins

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -1,6 +1,48 @@
 import type { Request, Response } from 'express';
 import * as adminService from '../services/admin_service';
 
+
+
+
+/**
+ * @swagger
+ * /admin/login:
+ *   post:
+ *     summary: Admin sign-in
+ *     tags: [Admin]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: admin@mail.com
+ *               password:
+ *                 type: string
+ *                 example: adminpass
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                     username:
+ *                       type: string
+ *       401:
+ *         description: Invalid credentials
+ */
 export const adminSignIn = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
