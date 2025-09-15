24683446551f351db3ec5691c85bcce7af321633
simon <spivakNak2@proton.me>
2025-09-15 06:25:06 +0000

    new

--- src/bundle-admin.ts
+++ src/bundle-admin.ts
@@ -1,10 +1,12 @@
-// import { bundle } from '@adminjs/bundler';
+ /*
+ import { bundle } from '@adminjs/bundler';
 
-// (async () => {
-//   await bundle({
-//     destinationDir: 'dist/adminjs-bundle', // Output to this dir (relative to CWD)
-//   });
-//   console.log('✅ AdminJS bundle generated in dist/adminjs-bundle');
-// })().catch(err => {
-//   console.error('❌ Bundle error:', err.message);
-// });
+ (async () => {
+   await bundle({
+     destinationDir: 'dist/adminjs-bundle', // Output to this dir (relative to CWD)
+   });
+   console.log('✅ AdminJS bundle generated in dist/adminjs-bundle');
+ })().catch(err => {
+   console.error('❌ Bundle error:', err.message);
+ });
+ */
